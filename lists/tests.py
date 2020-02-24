95ad2e450ebde7a2036f100029169de74ec3d9e0
Ramy Mousa <ramy_master99@hotmail.com>
2020-02-24 20:34:35 +0200

    Model for list Items and associated miigration

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,8 @@
-from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -18,3 +18,21 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, first_item.text)
+        self.assertEqual(second_saved_item.text, second_item.text)
